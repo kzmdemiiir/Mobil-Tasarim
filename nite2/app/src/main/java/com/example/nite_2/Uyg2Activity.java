920822019c6dd26d029e5eea279b065d17f38c8f
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-09-20 16:52:15 +0300

    no message

---
+++ nite2/app/src/main/java/com/example/nite_2/Uyg2Activity.java
@@ -0,0 +1,15 @@
+package com.example.nite_2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Uyg2Activity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uyg2);
+    }
+}
